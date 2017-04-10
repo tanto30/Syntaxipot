722ff699b365cc7dd8b14e54a064b85a65bc5fb2
Tamir Tuch <tamirtuch@mail.tau.ac.il>
2017-04-10 23:26:00 +0300

    Added CGIHandling, Request class, embedded both in HTTPHandler

--- CGIHandler.py
+++ CGIHandler.py
@@ -5,24 +5,34 @@ from subprocess import check_output
 
 
 def sanitize(shell_command):
-    # TODO: fix sanitize to not remove relavent query string input.
+    # TODO: make sanitize as secure as possible
     """
     :param shell_command: unsanitized string
-    :return: string containing only alphanumeric characters, periods, underscores, dashs
+    :return: string containing only alphanumeric characters, periods, underscores, dashes, equal marks
     """
-    return re.sub('[^\w._\-]', '', shell_command)
+    return re.sub('[^\w._\-\=]', '', shell_command)
 
 
 def handle_generic(request_obj):
+    """
+    Handles all other file types, (e.g., .htm, .html, .txt)
+    :param request_obj:  Request class from Request.py
+    :return: string of file contents.
+    """
     with open(request_obj.path, "rb") as f:
         file_contents = f.read()
     return file_contents
 
 
 def handle_php(request_obj):
-    params_str = sanitize(request_obj.query_parameters_str)
-    params = params_str.split('&')
-    return check_output(['php-cgi', '-f', request_obj.path, *params])
+    """
+    :param request_obj:  Request class from Request.py
+    :return: string of generated html file by executing php.
+    """
+    query_params = sanitize(request_obj.query_params)
+    params = query_params.split('&')
+    sanitized_params = [sanitize(param) for param in params]
+    return check_output(['php-cgi', '-f', request_obj.path, *sanitized_params])
 
 
 def serve_file(request_obj):
--- Request.py
+++ Request.py
@@ -1,6 +1,4 @@
-import email
-from io import StringIO
-
+import urllib.parse as urlparse
 
 class Request:
     """
@@ -9,31 +7,41 @@ class Request:
     This class does not have either queries or methods (except for the constructor), it contains only attributes.
 
     The class' attributes:
+    ip - the ip of the client making the request
+    port - the port of the client's socket which requested the server
     raw_request - the raw HTTP request.
-    method - The method that was used (e.g GET, POST etc.).
+    method - The method that was used (e.g. GET, POST etc.).
     path - The requested file (e.g /docs/index.html).
     query_params - The parameters of the query. For example, in the request 'GET /docs/index.html?qa=1&qa=2 HTTP/1.1'
                    the query_params is 'qa=1&qa=2'.
     headers - A dictionary that represents the headers of the request.
-    POST_params - The parameters of the POST request (they appear in the bottom of a HTTP request).
+    POST_params_str - The parameters of the POST request as string(e.g. user=admin&pass=123).
+    POST_params_dict - The  parameters of the POST request as dict(param:value, e.g. {'user':'admin', 'pass':'123'})
     """
 
-    def __init__(self, raw_request):
+    def __init__(self, ip, port, raw_request, method, path, headers, post_parameters, post_dict):
+        """
+        
+        :param ip: e.g. '127.0.0.1'
+        :param port: e.g. 5912 (unsure if int or str)
+        :param raw_request: whole HTTP Request, with request line, headers and body. (string)
+        :param method: e.g. 'GET' 
+        :param path: whole path of url <path>;<params>?<query>#<fragment>
+        :param headers: BaseHTTPRequestHandler.headers 
+        :param post_parameters: e.g. user=admin&pass=123
+        :param post_dict:  e.g. {'user':'admin', 'pass':'123'
+        """
+        self.ip = ip
+        self.port = port
         self.raw_request = raw_request
+        self.method = method
+        # Parse the url into 6 components  <scheme>://<netloc>/<path>;<params>?<query>#<fragment>
+        url_parsed = urlparse.urlparse(path)
+        self.path = url_parsed.path
+        self.query_params = url_parsed.query
 
-        request_line, rest_of_request = raw_request.split('\r\n', 1)
-        raw_headers, self.POST_params = rest_of_request.split("\r\n\r\n", 1)
-
-        split_request_line = request_line.split(" ")
-        self.method, query = split_request_line[0], split_request_line[1]
-        query_split = query.split("?")
-        self.path = query_split[0]
-        self.query_params = ""
-        if len(query_split) > 1:
-            self.query_params = query_split[1]
-
-        # construct a message from the request string
-        message = email.message_from_file(StringIO(raw_headers))
+        self.POST_params_str = post_parameters
+        self.POST_params_dict = post_dict
 
         # construct a dictionary containing the headers
-        self.headers = dict(message.items())
+        self.headers = dict(headers.items())
