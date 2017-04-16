5a95c796064ef10c4d49908b35cedf7765731c36
yoavarad <yoavarad10@gmail.com>
2017-04-16 17:32:16 +0300

    Update PageGenerator.py

--- PageGenerator.py
+++ PageGenerator.py
@@ -5,6 +5,8 @@ def generate_page(query):
 	file_name = "fake_page_"+str(hash(query))
 	if not os.path.isfile(file_name):
 		with open(file_name, "w") as f:
-			page = open("fake_page.txt").read()
+			fpage = open("fake_page.txt")
+			page = fpage.read()
 			f.write(page.format(query=query))
-
+			fpage.close()
+	return file_name
