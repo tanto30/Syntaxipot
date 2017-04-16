b1d0dc8671982ef607bfe9eede90647348e6bb93
yoav arad <yoav arad>
2017-04-16 17:03:43 +0300

    Fake page generator + template

--- PageGenerator.py
+++ PageGenerator.py
@@ -1,2 +1,10 @@
-def generate_page(request):
-    return ""
+import os.path
+
+
+def generate_page(query):
+	file_name = "fake_page_"+str(hash(query))
+	if not os.path.isfile(file_name):
+		with open(file_name, "w") as f:
+			page = open("fake_page.txt").read()
+			f.write(page.format(query=query))
+
