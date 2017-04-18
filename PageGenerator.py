4b1fa69b65efdb1ff8ad83b868732ae90da4579b
yoavarad <yoavarad10@gmail.com>
2017-04-18 16:57:34 +0300

    Now it works - next time if it ain't broke don't fix it

--- PageGenerator.py
+++ PageGenerator.py
@@ -2,18 +2,17 @@ import os.path
 
 
 def sqli_error_page(query):
-    file_name = "fake_page_" + str(hash(query))
-    if not os.path.isfile(file_name):
-        with open(file_name, "rw") as f:
-            fpage = open("fake_page.txt")
-            page = fpage.read()
-            f.write(page.format(query=query))
-            fpage.close()
+    file_name = "fake_sqli_error_page.html"
+    with open(file_name, "w") as f:
+        fpage = open("fake_page.txt")
+        page = fpage.read()
+        f.write(page.format(query=query))
+        fpage.close()
     return file_name
 
 
 def generate_page_filename(query, vuln_type):
     if vuln_type == "sqli":
-        filename = sqli_error_page(query)
+        return sqli_error_page(query)
     else:
         return "fake_page.txt"
