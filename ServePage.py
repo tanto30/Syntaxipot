3668425162a77d67dbfd50fe55179f780b31dff5
Tamir Tuch <tamirtuch@mail.tau.ac.il>
2017-04-23 19:25:53 +0300

    Changed serve_page response to bytes, now works on all content types. REMAINING BUG: Script raises urllib.error.HTTPError on response with response code other than 2XX, for example for response: 404 Not Found, script will crash and thus a 500 Internal Server Error will be returned (which shouldn't be, fix this.)

--- ServePage.py
+++ ServePage.py
@@ -2,30 +2,31 @@ import urllib.request
 
 
 def headers_dict_to_str(header_dict):
-	header_str = ''
-	for field, value in header_dict.items():
-		header_str += '{}: {}\r\n'.format(field, value)
-	return header_str
+    header_str = ''
+    for field, value in header_dict.items():
+        header_str += '{}: {}\r\n'.format(field, value)
+    return bytes(header_str, encoding='utf-8')
 
 
-def response_to_str(resp):
-	headers = headers_dict_to_str(dict(resp.info()))
-	body = str(resp.read(), encoding='utf-8')
-	return headers + '\r\n' + body
+def response_to_bytes(resp):
+    headers_bytes = headers_dict_to_str(dict(resp.info()))
+    body = resp.read()
+    return headers_bytes + b'\r\n' + body
 
 
 def serve_page(request_obj):
-	"""
-	:param request_obj: instance of Request.ClassifiedRequest
-	:return: HTTP Response for the request as str 
-	"""
-	# Full path to requested file
-	url = "http://127.0.0.1" + request_obj.return_path
-	# POST data as bytes object, None if request_obj.method = 'GET'
-	data = request_obj.POST_params_bytes
-	req = urllib.request.Request(url, data,
-								 request_obj.headers,
-								 method=request_obj.method)
-	resp = urllib.request.urlopen(req)
-	resp_str = response_to_str(resp)
-	return resp_str
+    """
+    :param request_obj: instance of Request.ClassifiedRequest
+    :return: HTTP Response for the request as str 
+    """
+    # Full path to requested file
+    url = "http://127.0.0.1" + request_obj.return_path
+    # POST data as bytes object, None if request_obj.method = 'GET'
+    data = request_obj.POST_params_bytes
+    req = urllib.request.Request(url, data,
+                                 request_obj.headers,
+                                 method=request_obj.method)
+    # TODO: Add HTTP Error handling, program crashes for example on HTTP Error 404
+    resp = urllib.request.urlopen(req)
+    resp_bytes = response_to_bytes(resp)
+    return resp_bytes
--- Syntaxipot.py
+++ Syntaxipot.py
@@ -1,4 +1,5 @@
 #! python
+import sys
 import Classifier
 import RequestGenerator
 from PageGenerator import generate_page_filename
@@ -7,30 +8,37 @@ from logger import log
 
 
 class HoneypotRequestHandler:
-	def __init__(self):
-		self.request = RequestGenerator.generate_request()
+    def __init__(self):
+        self.request = RequestGenerator.generate_request()
 
-	def __handle_malicious(self, classified_request):
-		log(classified_request)
-		page_name = generate_page_filename(self.request.query_params,
-										   classified_request.attack_type)
-		return page_name
+    def __handle_malicious(self, classified_request):
+        log(classified_request)
+        page_name = generate_page_filename(self.request.query_params,
+                                           classified_request.attack_type)
+        return page_name
 
-	def __handle_legitimate(self):
-		return self.request.path
+    def __handle_legitimate(self):
+        return self.request.path
 
-	def handle_request(self):
-		classified_request = Classifier.classify(self.request)
-		if classified_request.is_malicious():
-			page_name = self.__handle_malicious(classified_request)
-		else:
-			page_name = self.__handle_legitimate()
-		classified_request.set_return_path(page_name)
-		return serve_page(classified_request)
+    def handle_request(self):
+        classified_request = Classifier.classify(self.request)
+        if classified_request.is_malicious():
+            page_name = self.__handle_malicious(classified_request)
+        else:
+            page_name = self.__handle_legitimate()
+        classified_request.set_return_path(page_name)
+        return serve_page(classified_request)
+
+
+def write_output(bytes_to_write):
+    sys.stdout.buffer.write(bytes_to_write)
+    sys.stdout.buffer.flush()
 
 
 def run():
-	handler = HoneypotRequestHandler()
-	print(handler.handle_request())
+    handler = HoneypotRequestHandler()
+    out = handler.handle_request()
+    write_output(out)
+
 
 run()
